770a9b767031809bf4e508fc0a5a33c4091675d6
Cajetero <om.cortez.2010@gmail.com>
2014-05-09 14:54:01 -0600

    the configuration files

---
+++ web.js
@@ -0,0 +1,34 @@
+
+var path = require('path');
+var express = require("express");
+var logfmt = require("logfmt");
+var app = express();
+
+app.use(logfmt.requestLogger());
+
+
+app.use(express.compress());
+
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'dist'))); 
+
+
+
+
+
+var port = Number(process.env.PORT || 5000);
+
+
+
+app.post('/*', function(request, response) {
+  response.redirect('/');
+});
+
+
+
+
+
+app.listen(port, function() {
+ // console.log("Listening on " + port);
+});
